0671365ca5a6dd502266cca9a1ff469f202ce75f
sammuho <sammuho@yahoo.co.uk>
2021-01-13 18:58:13 +0000

    UPDAT

--- first.py
+++ first.py
@@ -1,15 +1,15 @@
-class Router():
+#
+class Router():  
     #router class
-    def _init_(self, model, swversion, ip_add):
+    def __init__(self, modell, swversion, ip_add):
         #initialize values
-        self.model = model
+        self.modell=modell
+        #self.model = model
         self.swversion = swversion
         self.ip_add = ip_add
     def getdesc(self):
         #return a formatted desc of the router
-        desc=f'Router Model: {self.model}\n'\
-            f'Software Version: {self.swversion}\n'\
-            f'Router Management Address:{self.ip_add}'
+        desc=f'Router Model : {self.modell}\n' f'Software Version : {self.swversion}\n'f'Router Management Address:{self.ip_add}'
         return desc
 rtr1=Router('iosV','15.6.7','10.10.10.1')
 rtr2=Router('isr4221','16.9.5','10.10.10.5')
---
+++ xception.py
@@ -0,0 +1,25 @@
+#syntax error missing or additional braces or brackets
+#handling exception
+class valueTooHighError(Exception):
+    pass
+#or add some more information for handlers
+class ValueTooLowError(Exception):
+    def __init__(self, message, value):
+        self.message= message
+        self.value=value
+def test_value(a):
+    if a > 1000:
+        raise valueTooHighError('Value too high') 
+    if a < 5:
+        #constructor takes 2 arguments
+        raise ValueTooLowError('Value too low', a) 
+    return a  
+try:
+    test_value(1001)
+except valueTooHighError as e:
+    print(e)
+except ValueTooLowError as e:
+    print(e.message, 'The value is:', e.value)
+else:
+    #test_value < 5 & > 1000:
+    print('Value within Range')   
